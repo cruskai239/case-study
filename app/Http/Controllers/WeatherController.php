0dd6e864bd49f5cbfc36799d6763f2a2ef537359
Chris <cruskai@24hrvirtualmd.com>
2017-09-19 19:42:41 -0400

    Get rid of cache clear command

---
+++ app/Http/Controllers/WeatherController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Chris
+ * Date: 9/19/2017
+ * Time: 7:36 PM
+ */
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WeatherController extends Controller
+{
+    public function post_GetWeatherData(Request $request){
+        
+
+    }
+
+}
