7f6622b9b49d286ac00d70ddadbc039ebc57ee8f
Chris <cruskai@24hrvirtualmd.com>
2017-09-21 14:54:52 -0400

    Added query string params for timeslice, start, and end date to be fed into ReportCriteria.

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-
 use App\Http\Requests\Services\Reporting\ReportRequest;
 use App\Services\Impressions\Impression;
 use App\Services\Reporting\ReportCriteria;
@@ -13,15 +12,17 @@ use Illuminate\Support\Facades\Auth;
 class ReportController extends Controller
 {
     //
-    public function post_GetReportData(ReportRequest $request){
+    public function post_GetReportData(ReportRequest $request)
+    {
         ini_set('memory_limit', '-1');
-        $reportCriteria = new ReportCriteria($request->input('eventNames', ['page_view']));
+        $startDate = $request->input('startDate', null);
+        $endDate = $request->input('endDate', null);
+        $timeslice = $request->input('timeslice', 30);
+        $reportCriteria =
+            new ReportCriteria($request->input('eventNames', ['page_view']), $startDate, $endDate, false, $timeslice);
         $result = $reportCriteria->runReport();
-
         Impression::create(['event_name' => 'report_generated', 'user_id' => Auth::user()->id]);
-
         return response()->json($result);
     }
-
 
 }
